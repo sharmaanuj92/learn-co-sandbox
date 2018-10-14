eeb999b25d673d387aaec9b002c89c97ff8b7982
sharmaanuj92 <sharmaanuj92@yahoo.com>
2018-10-14 20:00:27 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -1,11 +1,11 @@
-def badge_
+def badge_maker
   new_arr = []
   array.each do |name|
-    new_arr << "My name is #{name}"
+    new_arr << "Hello, my name is #{name}"
   end
   return new_arr
 end
 
-my_array = ["Humzah", "Jordan", "Jessica"]
+my_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 puts my_method(my_array)
