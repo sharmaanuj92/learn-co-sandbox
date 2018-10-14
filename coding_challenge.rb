c7005d550261710506094e61aa8a44a1d52faa30
sharmaanuj92 <sharmaanuj92@yahoo.com>
2018-10-14 20:36:23 +0000

    automatically backed up by learn

--- coding_challenge.rb
+++ coding_challenge.rb
@@ -1,14 +1,12 @@
+
 def batch_badge_creator(array)
+  x = 1
+  x += 1 
   new_arr = []
   array.each do |name|
     new_arr << "Hello, #{name}! You'll be assigned to room #{x}"
   end
   return new_arr
-  x = 1 
-  if x <= 7
-    puts "Hello, #{name}! You'll be assigned to room #{x}"
-  end 
-  x += 1
 end
 
 my_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
