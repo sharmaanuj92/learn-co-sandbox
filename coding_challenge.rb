56c8ae9130925e170dfb63351bfd461c5695985b
sharmaanuj92 <sharmaanuj92@yahoo.com>
2018-10-14 20:17:39 +0000

    automatically backed up by learn

---
+++ coding_challenge.rb
@@ -0,0 +1,13 @@
+def batch_badge_creator(array)
+  x = 1 
+  
+  new_arr = []
+  array.each do |name|
+    new_arr << "Hello, my name is #{name}"
+  end
+  return new_arr
+end
+
+my_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+puts batch_badge_creator(my_array)
